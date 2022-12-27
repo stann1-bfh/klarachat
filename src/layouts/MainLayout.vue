66ad0843b59605374e12d4b0b956577f7cbf2bde
Inkogniko <nikola.stanic@students.bfh.ch>
2022-12-27 17:46:58 +0100

    Title Change

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,7 +8,7 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
+          Klarachat
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
